2a8a2e5d2f9732a2274831ca3f8c3db6a05150d6
Dominic Orchard <dom.orchard@gmail.com>
2014-08-20 11:48:52 +0100

    subspaces

--- algebra.hs
+++ algebra.hs
@@ -1,11 +1,17 @@
-{-# LANGUAGE TypeFamilies, FlexibleInstances, NoMonomorphismRestriction #-}
-
+{-# LANGUAGE TypeFamilies, FlexibleInstances, NoMonomorphismRestriction, 
+             MultiParamTypeClasses #-}
 
 import GHC.Arr
 
 {- All containers must be containers for stencil computations -}
 
 import Control.Comonad
+
+{- Provides:
+class Functor c => Comonad c where
+   extract :: c a -> a
+   extend :: (c a -> b) -> c a -> c b
+-}
 
 -- Some alises for niceness
 
@@ -26,24 +32,24 @@ neighbours = (fmap (extract . fst)) . neighbourhood
 
 {- <grid> -}
 
-data Grid i a = Grid (Array i a) i deriving Show
+data Grid i a = Grid (Array i a) i (i, i) deriving Show
 
 instance Ix i => Functor (Grid i) where
-    fmap f (Grid a i) = Grid (amap f a) i
+    fmap f (Grid a i b) = Grid (amap f a) i b
 
 instance Ix i => Comonad (Grid i) where
-    extract (Grid a c) = a ! c
-    extend k (Grid a c) = let es' = map (\j -> (j, k (Grid a j))) (indices a)
-                          in Grid (array (bounds a) es') c
+    extract (Grid a c b) = a ! c
+    extend k (Grid a c b) = let es' = map (\j -> (j, k (Grid a j b))) (indices a)
+                            in Grid (array (bounds a) es') c b
 
 instance Neighbourhood (Grid Int) where
     type Marker (Grid Int) = Int
-    neighbourhood (Grid a i) = 
+    neighbourhood (Grid a i b) = 
         let (l, u) = bounds a
-        in  if (i == l)               then [(Grid a l, 0), (Grid a (l + 1), 1)]
-            else if (i == u)          then [(Grid a (u - 1), -1), (Grid a u, 0)]
-            else if (i > l && i < u)  then [(Grid a (i - 1), -1), (Grid a i, 0),
-                                            (Grid a (i + 1), 1)]
+        in  if (i == l)               then [(Grid a l b, 0), (Grid a (l + 1) b, 1)]
+            else if (i == u)          then [(Grid a (u - 1) b, -1), (Grid a u b, 0)]
+            else if (i > l && i < u)  then [(Grid a (i - 1) b, -1), (Grid a i b, 0),
+                                            (Grid a (i + 1) b, 1)]
             else []
 
 {- </grid> -}
@@ -52,4 +58,29 @@ instance Neighbourhood (Grid Int) where
 
 localMean g = (sum $ neighbours g) / (fromInteger . toInteger $ length (neighbours g))
 
-foo = promote localMean (Grid (listArray (0 :: Int, 4) [1.0, 4.0, 2.0, 4.0, 1.0]) 0)
+example1 = promote localMean (Grid (listArray (0 :: Int, 4) [1.0, 4.0, 2.0, 4.0, 1.0]) 0 (0, 4))
+
+class SubspaceComonad c i where
+    {- Relative comonad on the category of endomorphisms -}
+    promoteSub :: (c i a -> a) -> c i a -> c i a
+
+    {- The following are comonad homomorphisms, restricting the context domain -}
+
+    -- Restrict the internal bounds to a subspace defined by a pair of lower-upper bounds
+    subspace :: c i a -> (i, i) -> c i a
+                
+    -- Drop the restriction of a subspace, go back to the full spaced
+    fullspace :: c i a -> c i a
+
+instance SubspaceComonad Grid Int where
+    promoteSub k (Grid a c (l, u)) = 
+           let es' = map (\j -> if (elem j [l..u]) then 
+                                    (j, k (Grid a j (l, u)))
+                                else 
+                                    (j, a ! j)) (indices a)
+           in Grid (array (bounds a) es') c (l, u)
+
+    subspace (Grid a i _) b = Grid a i b
+    fullspace (Grid a i _)  = Grid a i (bounds a)
+        
+    
