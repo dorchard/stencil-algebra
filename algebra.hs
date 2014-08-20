e2037f1106b616c5a3592f7501a96b1667e1b191
Dominic Orchard <dom.orchard@gmail.com>
2014-08-20 11:01:38 +0100

    simple example based around comonad + neighbourhood coalgebra, arrays with locamean

---
+++ algebra.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE TypeFamilies, FlexibleInstances, NoMonomorphismRestriction #-}
+
+
+import GHC.Arr
+
+{- All containers must be containers for stencil computations -}
+
+import Control.Comonad
+
+{- All containers must have a neighbourhood coalgebra -}
+
+class Neighbourhood g where
+    type Marker g 
+    neighbourhood :: g a -> [(g a, Marker g)]
+
+neighbours :: (Neighbourhood g, Comonad g) => g a -> [a]
+neighbours = (fmap (extract . fst)) . neighbourhood
+
+{- <grid> -}
+
+data Grid i a = Grid (Array i a) i
+
+instance Ix i => Functor (Grid i) where
+    fmap f (Grid a i) = Grid (amap f a) i
+
+instance Ix i => Comonad (Grid i) where
+    extract (Grid a c) = a ! c
+    extend k (Grid a c) = let es' = map (\j -> (j, k (Grid a j))) (indices a)
+                          in Grid (array (bounds a) es') c
+
+instance Neighbourhood (Grid Int) where
+    type Marker (Grid Int) = Int
+    neighbourhood (Grid a i) = 
+        let (l, u) = bounds a
+        in  if (i == l)               then [(Grid a l, 0), (Grid a (l + 1), 1)]
+            else if (i == u)          then [(Grid a (u - 1), -1), (Grid a u, 0)]
+            else if (i > l && i < u)  then [(Grid a (i - 1), -1), (Grid a i, 0),
+                                            (Grid a (i + 1), 1)]
+            else []
+
+{- </grid> -}
+
+-- Example
+
+localMean g = (sum $ neighbours g) / (fromInteger . toInteger $ length (neighbours g))
